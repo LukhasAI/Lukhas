715f0d0c0d218de40c2c5220cf7fac9dd61a6eab
GRDM  -  LUKHΛS   ΛI <206150622+LukhasAI@users.noreply.github.com>
2025-11-03 17:46:38 +0000

    bridge(core): add bridge exports for labs modules

--- core/__init__.py
+++ core/__init__.py
@@ -3,3 +3,753 @@ Core module for LUKHAS - foundational systems and utilities.
 """
 # Make this a proper package after lukhas/ namespace removal
 __all__ = []
+
+# Bridge export for core.GlobalInstitutionalCompliantEngine
+try:
+    from labs.core import GlobalInstitutionalCompliantEngine
+except ImportError:
+    def GlobalInstitutionalCompliantEngine(*args, **kwargs):
+        '''Stub for GlobalInstitutionalCompliantEngine.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "GlobalInstitutionalCompliantEngine" not in __all__:
+    __all__.append("GlobalInstitutionalCompliantEngine")
+
+# Bridge export for core.GlobalInstitutionalFramework
+try:
+    from labs.core import GlobalInstitutionalFramework
+except ImportError:
+    def GlobalInstitutionalFramework(*args, **kwargs):
+        '''Stub for GlobalInstitutionalFramework.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "GlobalInstitutionalFramework" not in __all__:
+    __all__.append("GlobalInstitutionalFramework")
+
+# Bridge export for core.MultiBrainSymphony
+try:
+    from labs.core import MultiBrainSymphony
+except ImportError:
+    def MultiBrainSymphony(*args, **kwargs):
+        '''Stub for MultiBrainSymphony.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "MultiBrainSymphony" not in __all__:
+    __all__.append("MultiBrainSymphony")
+
+# Bridge export for core.accent_adapter
+try:
+    from labs.core import accent_adapter
+except ImportError:
+    def accent_adapter(*args, **kwargs):
+        '''Stub for accent_adapter.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "accent_adapter" not in __all__:
+    __all__.append("accent_adapter")
+
+# Bridge export for core.agi_security
+try:
+    from labs.core import agi_security
+except ImportError:
+    def agi_security(*args, **kwargs):
+        '''Stub for agi_security.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "agi_security" not in __all__:
+    __all__.append("agi_security")
+
+# Bridge export for core.ai_compliance
+try:
+    from labs.core import ai_compliance
+except ImportError:
+    def ai_compliance(*args, **kwargs):
+        '''Stub for ai_compliance.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "ai_compliance" not in __all__:
+    __all__.append("ai_compliance")
+
+# Bridge export for core.auth
+try:
+    from labs.core import auth
+except ImportError:
+    def auth(*args, **kwargs):
+        '''Stub for auth.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "auth" not in __all__:
+    __all__.append("auth")
+
+# Bridge export for core.cognitive_security
+try:
+    from labs.core import cognitive_security
+except ImportError:
+    def cognitive_security(*args, **kwargs):
+        '''Stub for cognitive_security.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "cognitive_security" not in __all__:
+    __all__.append("cognitive_security")
+
+# Bridge export for core.collaborative_ai_agent_system
+try:
+    from labs.core import collaborative_ai_agent_system
+except ImportError:
+    def collaborative_ai_agent_system(*args, **kwargs):
+        '''Stub for collaborative_ai_agent_system.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "collaborative_ai_agent_system" not in __all__:
+    __all__.append("collaborative_ai_agent_system")
+
+# Bridge export for core.compliance_registry
+try:
+    from labs.core import compliance_registry
+except ImportError:
+    def compliance_registry(*args, **kwargs):
+        '''Stub for compliance_registry.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "compliance_registry" not in __all__:
+    __all__.append("compliance_registry")
+
+# Bridge export for core.confidence_calibrator
+try:
+    from labs.core import confidence_calibrator
+except ImportError:
+    def confidence_calibrator(*args, **kwargs):
+        '''Stub for confidence_calibrator.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "confidence_calibrator" not in __all__:
+    __all__.append("confidence_calibrator")
+
+# Bridge export for core.constitutional_safety
+try:
+    from labs.core import constitutional_safety
+except ImportError:
+    def constitutional_safety(*args, **kwargs):
+        '''Stub for constitutional_safety.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "constitutional_safety" not in __all__:
+    __all__.append("constitutional_safety")
+
+# Bridge export for core.core
+try:
+    from labs.core import core
+except ImportError:
+    def core(*args, **kwargs):
+        '''Stub for core.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "core" not in __all__:
+    __all__.append("core")
+
+# Bridge export for core.creative_expressions
+try:
+    from labs.core import creative_expressions
+except ImportError:
+    def creative_expressions(*args, **kwargs):
+        '''Stub for creative_expressions.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "creative_expressions" not in __all__:
+    __all__.append("creative_expressions")
+
+# Bridge export for core.creative_personality
+try:
+    from labs.core import creative_personality
+except ImportError:
+    def creative_personality(*args, **kwargs):
+        '''Stub for creative_personality.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "creative_personality" not in __all__:
+    __all__.append("creative_personality")
+
+# Bridge export for core.credential_manager
+try:
+    from labs.core import credential_manager
+except ImportError:
+    def credential_manager(*args, **kwargs):
+        '''Stub for credential_manager.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "credential_manager" not in __all__:
+    __all__.append("credential_manager")
+
+# Bridge export for core.crypto
+try:
+    from labs.core import crypto
+except ImportError:
+    def crypto(*args, **kwargs):
+        '''Stub for crypto.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "crypto" not in __all__:
+    __all__.append("crypto")
+
+# Bridge export for core.defense_monitor
+try:
+    from labs.core import defense_monitor
+except ImportError:
+    def defense_monitor(*args, **kwargs):
+        '''Stub for defense_monitor.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "defense_monitor" not in __all__:
+    __all__.append("defense_monitor")
+
+# Bridge export for core.dream_vocabulary
+try:
+    from labs.core import dream_vocabulary
+except ImportError:
+    def dream_vocabulary(*args, **kwargs):
+        '''Stub for dream_vocabulary.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "dream_vocabulary" not in __all__:
+    __all__.append("dream_vocabulary")
+
+# Bridge export for core.emotion_mapper
+try:
+    from labs.core import emotion_mapper
+except ImportError:
+    def emotion_mapper(*args, **kwargs):
+        '''Stub for emotion_mapper.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "emotion_mapper" not in __all__:
+    __all__.append("emotion_mapper")
+
+# Bridge export for core.emotion_mapper_alt
+try:
+    from labs.core import emotion_mapper_alt
+except ImportError:
+    def emotion_mapper_alt(*args, **kwargs):
+        '''Stub for emotion_mapper_alt.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "emotion_mapper_alt" not in __all__:
+    __all__.append("emotion_mapper_alt")
+
+# Bridge export for core.emotion_trend_tracker
+try:
+    from labs.core import emotion_trend_tracker
+except ImportError:
+    def emotion_trend_tracker(*args, **kwargs):
+        '''Stub for emotion_trend_tracker.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "emotion_trend_tracker" not in __all__:
+    __all__.append("emotion_trend_tracker")
+
+# Bridge export for core.emotional_oscillator
+try:
+    from labs.core import emotional_oscillator
+except ImportError:
+    def emotional_oscillator(*args, **kwargs):
+        '''Stub for emotional_oscillator.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "emotional_oscillator" not in __all__:
+    __all__.append("emotional_oscillator")
+
+# Bridge export for core.enhanced_crypto
+try:
+    from labs.core import enhanced_crypto
+except ImportError:
+    def enhanced_crypto(*args, **kwargs):
+        '''Stub for enhanced_crypto.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "enhanced_crypto" not in __all__:
+    __all__.append("enhanced_crypto")
+
+# Bridge export for core.entropy_probe
+try:
+    from labs.core import entropy_probe
+except ImportError:
+    def entropy_probe(*args, **kwargs):
+        '''Stub for entropy_probe.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "entropy_probe" not in __all__:
+    __all__.append("entropy_probe")
+
+# Bridge export for core.experience_manager
+try:
+    from labs.core import experience_manager
+except ImportError:
+    def experience_manager(*args, **kwargs):
+        '''Stub for experience_manager.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "experience_manager" not in __all__:
+    __all__.append("experience_manager")
+
+# Bridge export for core.federated_integrator
+try:
+    from labs.core import federated_integrator
+except ImportError:
+    def federated_integrator(*args, **kwargs):
+        '''Stub for federated_integrator.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "federated_integrator" not in __all__:
+    __all__.append("federated_integrator")
+
+# Bridge export for core.hash
+try:
+    from labs.core import hash
+except ImportError:
+    def hash(*args, **kwargs):
+        '''Stub for hash.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "hash" not in __all__:
+    __all__.append("hash")
+
+# Bridge export for core.kms_manager
+try:
+    from labs.core import kms_manager
+except ImportError:
+    def kms_manager(*args, **kwargs):
+        '''Stub for kms_manager.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "kms_manager" not in __all__:
+    __all__.append("kms_manager")
+
+# Bridge export for core.main_bot
+try:
+    from labs.core import main_bot
+except ImportError:
+    def main_bot(*args, **kwargs):
+        '''Stub for main_bot.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "main_bot" not in __all__:
+    __all__.append("main_bot")
+
+# Bridge export for core.main_loop
+try:
+    from labs.core import main_loop
+except ImportError:
+    def main_loop(*args, **kwargs):
+        '''Stub for main_loop.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "main_loop" not in __all__:
+    __all__.append("main_loop")
+
+# Bridge export for core.multi_agent_consensus
+try:
+    from labs.core import multi_agent_consensus
+except ImportError:
+    def multi_agent_consensus(*args, **kwargs):
+        '''Stub for multi_agent_consensus.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "multi_agent_consensus" not in __all__:
+    __all__.append("multi_agent_consensus")
+
+# Bridge export for core.neural_intelligence_api
+try:
+    from labs.core import neural_intelligence_api
+except ImportError:
+    def neural_intelligence_api(*args, **kwargs):
+        '''Stub for neural_intelligence_api.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "neural_intelligence_api" not in __all__:
+    __all__.append("neural_intelligence_api")
+
+# Bridge export for core.neural_intelligence_main
+try:
+    from labs.core import neural_intelligence_main
+except ImportError:
+    def neural_intelligence_main(*args, **kwargs):
+        '''Stub for neural_intelligence_main.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "neural_intelligence_main" not in __all__:
+    __all__.append("neural_intelligence_main")
+
+# Bridge export for core.neural_symbolic_bridge
+try:
+    from labs.core import neural_symbolic_bridge
+except ImportError:
+    def neural_symbolic_bridge(*args, **kwargs):
+        '''Stub for neural_symbolic_bridge.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "neural_symbolic_bridge" not in __all__:
+    __all__.append("neural_symbolic_bridge")
+
+# Bridge export for core.personality
+try:
+    from labs.core import personality
+except ImportError:
+    def personality(*args, **kwargs):
+        '''Stub for personality.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "personality" not in __all__:
+    __all__.append("personality")
+
+# Bridge export for core.personality_refiner
+try:
+    from labs.core import personality_refiner
+except ImportError:
+    def personality_refiner(*args, **kwargs):
+        '''Stub for personality_refiner.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "personality_refiner" not in __all__:
+    __all__.append("personality_refiner")
+
+# Bridge export for core.personas
+try:
+    from labs.core import personas
+except ImportError:
+    def personas(*args, **kwargs):
+        '''Stub for personas.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "personas" not in __all__:
+    __all__.append("personas")
+
+# Bridge export for core.quantum_financial_consciousness_engine
+try:
+    from labs.core import quantum_financial_consciousness_engine
+except ImportError:
+    def quantum_financial_consciousness_engine(*args, **kwargs):
+        '''Stub for quantum_financial_consciousness_engine.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "quantum_financial_consciousness_engine" not in __all__:
+    __all__.append("quantum_financial_consciousness_engine")
+
+# Bridge export for core.secure_logging
+try:
+    from labs.core import secure_logging
+except ImportError:
+    def secure_logging(*args, **kwargs):
+        '''Stub for secure_logging.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "secure_logging" not in __all__:
+    __all__.append("secure_logging")
+
+# Bridge export for core.security_integration
+try:
+    from labs.core import security_integration
+except ImportError:
+    def security_integration(*args, **kwargs):
+        '''Stub for security_integration.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "security_integration" not in __all__:
+    __all__.append("security_integration")
+
+# Bridge export for core.security_policy
+try:
+    from labs.core import security_policy
+except ImportError:
+    def security_policy(*args, **kwargs):
+        '''Stub for security_policy.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "security_policy" not in __all__:
+    __all__.append("security_policy")
+
+# Bridge export for core.self_reflect_cron
+try:
+    from labs.core import self_reflect_cron
+except ImportError:
+    def self_reflect_cron(*args, **kwargs):
+        '''Stub for self_reflect_cron.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "self_reflect_cron" not in __all__:
+    __all__.append("self_reflect_cron")
+
+# Bridge export for core.signal_middleware
+try:
+    from labs.core import signal_middleware
+except ImportError:
+    def signal_middleware(*args, **kwargs):
+        '''Stub for signal_middleware.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "signal_middleware" not in __all__:
+    __all__.append("signal_middleware")
+
+# Bridge export for core.signals
+try:
+    from labs.core import signals
+except ImportError:
+    def signals(*args, **kwargs):
+        '''Stub for signals.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "signals" not in __all__:
+    __all__.append("signals")
+
+# Bridge export for core.symbolic_tuner
+try:
+    from labs.core import symbolic_tuner
+except ImportError:
+    def symbolic_tuner(*args, **kwargs):
+        '''Stub for symbolic_tuner.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "symbolic_tuner" not in __all__:
+    __all__.append("symbolic_tuner")
+
+# Bridge export for core.the_oscillator
+try:
+    from labs.core import the_oscillator
+except ImportError:
+    def the_oscillator(*args, **kwargs):
+        '''Stub for the_oscillator.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "the_oscillator" not in __all__:
+    __all__.append("the_oscillator")
+
+# Bridge export for core.voice_vocabulary
+try:
+    from labs.core import voice_vocabulary
+except ImportError:
+    def voice_vocabulary(*args, **kwargs):
+        '''Stub for voice_vocabulary.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "voice_vocabulary" not in __all__:
+    __all__.append("voice_vocabulary")
+
+# Bridge export for core.vulnerability_dashboard
+try:
+    from labs.core import vulnerability_dashboard
+except ImportError:
+    def vulnerability_dashboard(*args, **kwargs):
+        '''Stub for vulnerability_dashboard.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "vulnerability_dashboard" not in __all__:
+    __all__.append("vulnerability_dashboard")
+
+# Bridge export for core.workflow_engine
+try:
+    from labs.core import workflow_engine
+except ImportError:
+    def workflow_engine(*args, **kwargs):
+        '''Stub for workflow_engine.'''
+        return None
+
+try:
+    __all__  # type: ignore[name-defined]
+except NameError:
+    __all__ = []
+if "workflow_engine" not in __all__:
+    __all__.append("workflow_engine")
